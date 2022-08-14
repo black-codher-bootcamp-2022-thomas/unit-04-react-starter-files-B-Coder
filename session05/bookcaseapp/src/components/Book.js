565e01c4d9f245f9462931a2d313473774c6b0fc
B-Coder <97311741+B-Coder@users.noreply.github.com>
2022-08-14 12:40:14 +0100

    destructing bookdata

--- session05/bookcaseapp/src/components/Book.js
+++ session05/bookcaseapp/src/components/Book.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Book = (props) => {
+const Book = ({title, author, price, description}) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.author}</p>
-      <p>{props.price}</p>
-      <p>{props.description}</p>
+    <div style={{ border: "4px solid indigo" }}>
+      <h1>{title}</h1>
+      <p>{author}</p>
+      <p>{price}</p>
+      <p>{description}</p>
+    
     </div>
   );
 };
