8970b3fcf503aaf7a5cadc0da3b491c8c57f506e
Steph Grant <stephgrant@Stephs-Mac-mini.local>
2022-08-23 19:19:30 +0100

    new react folder

---
+++ task1-session09/src/App.js
@@ -0,0 +1,11 @@
+import Car from "Component./Car";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Car />
+    </div>
+  );
+}
+
+
---
+++ task1-session09/src/components/Car.js
@@ -0,0 +1,3 @@
+export default function Car() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
